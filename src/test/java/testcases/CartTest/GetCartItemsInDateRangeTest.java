1b6098b5c2a0ffd40abe9b75e5274e9dd7bf6a10
Injas Mahendra Berutu <injasmahendra123@gmail.com>
2025-05-13 10:39:20 +0700

    update cart testcases

---
+++ src/test/java/testcases/CartTest/GetCartItemsInDateRangeTest.java
@@ -0,0 +1,11 @@
+package testcases.CartTest;
+
+import base.TestBase;
+import org.testng.annotations.Test;
+
+public class GetCartItemsInDateRangeTest extends TestBase {
+    @Test
+    public void getCartItemsInDateRangeTest(){
+
+    }
+}
